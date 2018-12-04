8248849a6bf47a4068d2bf4860c5a641913d7c6b
Benjabling <groom.benjamin@gmail.com>
2018-12-04 00:41:37 -0800

    Day 3 not complete

--- 3/3.py
+++ 3/3.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 import re
-
+from itertools import product
 # Fetch data
 
 myfile = open("input", 'r')
@@ -16,4 +16,15 @@ fabric = [0 for x in range(0,1000), 0 for y in range(0,1000)]
 # Populate fabric with planned cuts
 
 for x in range(0, len(contents)):
-    print contents[x]
+
+    # Put dimensions in variables
+
+    nums = re.split('(\d+)', contents[x], maxsplit=5)
+    xmax = int(nums[3]) + int(nums[7])
+    xmin = int(nums[3])
+    ymax = int(nums[5]) + int(nums[9])
+    ymin = int(nums[5])
+
+    # Mark dimensions on fabric
+
+    	
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 advent 2018
+
+This year I'll be completing each day the same day the challenge is issued, possibly using some inspiration from adventofcode.reddit.comto learn about unknown-to-me syntax, modules and data structures in C or Python 2. I may also explore other languages here and there, especially since elegant, odd and interesting solutions show up so often on reddit. A humbling experience to be sure. 
+
+GLHF - bjgroom
