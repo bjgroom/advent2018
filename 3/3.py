e130d6a2b0b5040b58d1c32efc472f61464213b6
Benjamin James-Groom <bgroom@uvic.ca>
2018-12-04 10:03:30 -0800

    Day 3 done

--- 3/3.py
+++ 3/3.py
@@ -9,9 +9,13 @@ contents = list()
 for line in myfile: contents.append(line.rstrip())
 myfile.close()
 
-# Build master grid
+# Build master grid in 2D list
 
-fabric = [0 for x in range(0,1000), 0 for y in range(0,1000)]
+fabric = []
+for x in range(0,1000):
+    fabric.append([])
+    for y in range(0,1000):
+	fabric[x].append(0)
 
 # Populate fabric with planned cuts
 
@@ -27,4 +31,43 @@ for x in range(0, len(contents)):
 
     # Mark dimensions on fabric
 
-    	
+    for v in range(ymin, ymax):
+	for h in range(xmin, xmax):
+	    fabric[h][v] += 1
+
+# TASK 3.1 ## Scan and count fabric for collisions (spaces with > 1 claim)
+
+bang = 0
+
+for v in range(0, 1000):
+    for h in range(0, 1000):
+	if fabric[h][v] > 1:
+	    bang += 1
+print bang
+
+# TASK 3.2 ## Re-scan fabric claim-by-claim to find ID with 0 collisions
+
+
+for x in range(0, len(contents)):
+
+    # Put dimensions in variables
+
+    nums = re.split('(\d+)', contents[x], maxsplit=5)
+    xmax = int(nums[3]) + int(nums[7])
+    xmin = int(nums[3])
+    ymax = int(nums[5]) + int(nums[9])
+    ymin = int(nums[5])
+
+    # Mark dimensions on fabric
+
+    flag = False
+
+    for v in range(ymin, ymax):
+	for h in range(xmin, xmax):
+	    if fabric[h][v] > 1:
+		flag = True
+
+    if flag == False:
+	print nums[1]
+	break
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-advent 2018
+#advent 2018#
 
-This year I'll be completing each day the same day the challenge is issued, possibly using some inspiration from adventofcode.reddit.comto learn about unknown-to-me syntax, modules and data structures in C or Python 2. I may also explore other languages here and there, especially since elegant, odd and interesting solutions show up so often on reddit. A humbling experience to be sure. 
+This year I'll be completing each day the same day the challenge is issued, quite likely using inspiration from the daily threads at adventofcode.reddit.com to learn about unknown-to-me syntax, modules and data structures in C or Python 2. I may also explore other languages here and there, especially since elegant, odd and interesting solutions show up so often on reddit. A humbling experience to be sure. 
 
 GLHF - bjgroom
